6b62a0dc9c6c3c86eb7f3014a5984722c9a2759a
regiapriandi012 <regiapriandi012@gmail.com>
2021-06-03 15:39:44 +0700

    mengedit file app

--- app.py
+++ app.py
@@ -13,11 +13,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def indexku():
-    return render_template ('index.html')
+    return render_template('index.html')
 
 @app.route("/index")
 def index():
-    return render_template ('index.html')
+    return render_template('index.html')
 
 @app.route("/model")
 def model():
