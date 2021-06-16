10cc50c0b1369570407705e5924141e07191a128
regiapriandi012 <regiapriandi012@gmail.com>
2021-06-16 15:34:30 +0700

    setting ulang app.py merubah hubber ke ridge

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template, request
 from .model.modelEmasLinear import lin_pred_future as el
 from .model.modelDolarLinear import lin_pred_future as dl
 from .model.modelPerakLinear import lin_pred_future as pl
-#from .model.modelEmasHubber import hub_pred_future as eh
-#from .model.modelDolarHubber import hub_pred_future as dh
-#from .model.modelPerakHubber import hub_pred_future as ph
+from .model.modelEmasRidge import rid_pred_future as er
+from .model.modelDolarRidge import rid_pred_future as dr
+from .model.modelPerakRidge import rid_pred_future as pr
 from .model.modelDolarLasso import las_pred_future as dla
 from .model.modelPerakLasso import las_pred_future as pla
 from .model.modelEmasLasso import las_pred_future as ela
@@ -33,14 +33,13 @@ def modelll():
     from .model.modelEmasLinear import coef as cel, intercept as iel
     from .model.modelPerakLinear import coef as cpl, intercept as ipl
     from .model.modelDolarLinear import coef as cdl, intercept as idl
-    #from .model.modelEmasHubber import coef as ceh, intercept as ieh
-    #from .model.modelDolarHubber import coef as cdh, intercept as idh
-    # from .model.modelPerakHubber import coef as cph, intercept as iph
+    from .model.modelEmasRidge import coef as cer, intercept as ier
+    from .model.modelDolarRidge import coef as cdr, intercept as idr
+    from .model.modelPerakRidge import coef as cpr, intercept as ipr
     from .model.modelDolarLasso import coef as cdla, intercept as idla
     from .model.modelEmasLasso import coef as cela, intercept as iela
     from .model.modelPerakLasso import coef as cpla, intercept as ipla
 
-    nama = request.form['nama']
     prediksi = request.form['prediksi']
     metode = request.form['metode']
 
@@ -61,7 +60,7 @@ def modelll():
         fig.savefig(buf, format="png")
 
         data = base64.b64encode(buf.getbuffer()).decode("ascii")
-        return render_template('model.html', data=data, nama=nama, prediksi=prediksi, metode=metode, hasil='Rp. {}'.format(round(float(el),2)), date=str(date.strftime("%A"))+', '+str(date.day)+'/'+str(date.month)+'/'+str(date.year), coef=cel, intercept=iel)
+        return render_template('model.html', data=data, prediksi=prediksi, metode=metode, hasil='Rp. {}'.format(float(el)), date=str(date.strftime("%A"))+', '+str(date.day)+'/'+str(date.month)+'/'+str(date.year), coef=cel, intercept=iel)
 
     elif metode == "Linear Regression" and prediksi == "Harga Perak":
         from .model.modelPerakLinear import df, dfe, lin_predict, lin_pred_future
@@ -80,7 +79,7 @@ def modelll():
         fig.savefig(buf, format="png")
 
         data = base64.b64encode(buf.getbuffer()).decode("ascii")
-        return render_template('model.html', data=data, nama=nama, prediksi=prediksi, metode=metode, hasil='Rp. {}'.format(round(float(pl),2)), date=str(date.strftime("%A"))+', '+str(date.day)+'/'+str(date.month)+'/'+str(date.year), coef=cpl, intercept=ipl)
+        return render_template('model.html', data=data, prediksi=prediksi, metode=metode, hasil='Rp. {}'.format(float(pl)), date=str(date.strftime("%A"))+', '+str(date.day)+'/'+str(date.month)+'/'+str(date.year), coef=cpl, intercept=ipl)
 
     elif metode == "Linear Regression" and prediksi == "Harga Dollar":
         from .model.modelDolarLinear import df, dfe, lin_predict, lin_pred_future
@@ -99,7 +98,7 @@ def modelll():
         fig.savefig(buf, format="png")
 
         data = base64.b64encode(buf.getbuffer()).decode("ascii")
-        return render_template('model.html', data=data, nama=nama, prediksi=prediksi, metode=metode, hasil='Rp. {}'.format(round(float(dl),2)), date=str(date.strftime("%A"))+', '+str(date.day)+'/'+str(date.month)+'/'+str(date.year), coef=cdl, intercept=idl)
+        return render_template('model.html', data=data, prediksi=prediksi, metode=metode, hasil='Rp. {}'.format(float(dl)), date=str(date.strftime("%A"))+', '+str(date.day)+'/'+str(date.month)+'/'+str(date.year), coef=cdl, intercept=idl)
 
     elif metode == "Lasso" and prediksi == "Harga Emas":
         from .model.modelEmasLasso import df, dfe, las_predict, las_pred_future
@@ -118,7 +117,7 @@ def modelll():
         fig.savefig(buf, format="png")
 
         data = base64.b64encode(buf.getbuffer()).decode("ascii")
-        return render_template('model.html', data=data, nama=nama, prediksi=prediksi, metode=metode, hasil='Rp. {}'.format(round(float(ela),2)), date=str(date.strftime("%A"))+', '+str(date.day)+'/'+str(date.month)+'/'+str(date.year), coef=cela, intercept=iela)
+        return render_template('model.html', data=data, prediksi=prediksi, metode=metode, hasil='Rp. {}'.format(float(ela)), date=str(date.strftime("%A"))+', '+str(date.day)+'/'+str(date.month)+'/'+str(date.year), coef=cela, intercept=iela)
 
     elif metode == "Lasso" and prediksi == "Harga Perak":
         from .model.modelPerakLasso import df, dfe, las_predict, las_pred_future
@@ -137,7 +136,7 @@ def modelll():
         fig.savefig(buf, format="png")
 
         data = base64.b64encode(buf.getbuffer()).decode("ascii")
-        return render_template('model.html', data=data, nama=nama, prediksi=prediksi, metode=metode, hasil='Rp. {}'.format(round(float(pla),2)), date=str(date.strftime("%A"))+', '+str(date.day)+'/'+str(date.month)+'/'+str(date.year), coef=cpla, intercept=ipla)
+        return render_template('model.html', data=data, prediksi=prediksi, metode=metode, hasil='Rp. {}'.format(float(pla)), date=str(date.strftime("%A"))+', '+str(date.day)+'/'+str(date.month)+'/'+str(date.year), coef=cpla, intercept=ipla)
 
     elif metode == "Lasso" and prediksi == "Harga Dollar":
         from .model.modelDolarLasso import df, dfe, las_predict, las_pred_future
@@ -156,66 +155,66 @@ def modelll():
         fig.savefig(buf, format="png")
 
         data = base64.b64encode(buf.getbuffer()).decode("ascii")
-        return render_template('model.html', data=data, nama=nama, prediksi=prediksi, metode=metode, hasil='Rp. {}'.format(round(float(dla),2)), date=str(date.strftime("%A"))+', '+str(date.day)+'/'+str(date.month)+'/'+str(date.year), coef=cdla, intercept=idla)
+        return render_template('model.html', data=data, prediksi=prediksi, metode=metode, hasil='Rp. {}'.format(float(dla)), date=str(date.strftime("%A"))+', '+str(date.day)+'/'+str(date.month)+'/'+str(date.year), coef=cdla, intercept=idla)
 
-    """
-    elif metode == "Hubber Regressor" and prediksi == "Harga Emas":
-        from .model.modelEmasHubber import df, dfe, hub_predict, hub_pred_future
+
+    elif metode == "Ridge Regression" and prediksi == "Harga Emas":
+        from .model.modelEmasRidge import df, dfe, rid_predict, rid_pred_future
         fig = Figure(figsize=(10, 8))
         ax = fig.subplots()
         ax.scatter(df.date, df['price'], color='green')
-        ax.plot(df.date, hub_predict)
-        ax.plot(dfe.prediksi, hub_pred_future)
+        ax.plot(df.date, rid_predict)
+        ax.plot(dfe.prediksi, rid_pred_future)
         ax.tick_params(labelrotation=30)
         ax.set_ylabel("Dalam Rupiah")
         ax.set_xlabel("Tanggal (jangka 14 hari)")
-        ax.legend(['Garis Huber Regressor'])
-        ax.set_title("Grafik Huber Regressor Prediksi Harga Emas")
+        ax.legend(['Garis Ridge Regression'])
+        ax.set_title("Grafik Ridge Regression Prediksi Harga Emas")
 
         buf = BytesIO()
         fig.savefig(buf, format="png")
     
         data = base64.b64encode(buf.getbuffer()).decode("ascii")
-        return render_template('model.html', data=data, nama=nama, prediksi=prediksi, metode=metode, hasil='Rp. {}'.format(round(int(eh),2)), date=str(date.strftime("%A"))+', '+str(date.day)+'/'+str(date.month)+'/'+str(date.year), coef=ceh, intercept=ieh)
+        return render_template('model.html', data=data, prediksi=prediksi, metode=metode, hasil='Rp. {}'.format(float(er)), date=str(date.strftime("%A"))+', '+str(date.day)+'/'+str(date.month)+'/'+str(date.year), coef=cer, intercept=ier)
         
-    elif metode == "Hubber Regressor" and prediksi == "Harga Perak":
-        from .model.modelPerakHubber import df, dfe, hub_predict, hub_pred_future
+    elif metode == "Ridge Regression" and prediksi == "Harga Perak":
+        from .model.modelPerakRidge import df, dfe, rid_predict, rid_pred_future
         fig = Figure(figsize=(10, 8))
         ax = fig.subplots()
         ax.scatter(df.date, df['price'], color='green')
-        ax.plot(df.date, hub_predict)
-        ax.plot(dfe.prediksi, hub_pred_future)
+        ax.plot(df.date, rid_predict)
+        ax.plot(dfe.prediksi, rid_pred_future)
         ax.tick_params(labelrotation=30)
         ax.set_ylabel("Dalam Rupiah")
         ax.set_xlabel("Tanggal (jangka 14 hari)")
-        ax.legend(['Garis Huber Regressor'])
-        ax.set_title("Grafik Huber Regressor Prediksi Harga Perak")
+        ax.legend(['Garis Ridge Regression'])
+        ax.set_title("Grafik Ridge Regression Prediksi Harga Perak")
     
         buf = BytesIO()
         fig.savefig(buf, format="png")
     
         data = base64.b64encode(buf.getbuffer()).decode("ascii")
-        return render_template('model.html', data=data, nama=nama, prediksi=prediksi, metode=metode, hasil='Rp. {}'.format(round(float(ph),2)), date=str(date.strftime("%A"))+', '+str(date.day)+'/'+str(date.month)+'/'+str(date.year), coef=cph, intercept=iph)
+        return render_template('model.html', data=data, prediksi=prediksi, metode=metode, hasil='Rp. {}'.format(float(pr)), date=str(date.strftime("%A"))+', '+str(date.day)+'/'+str(date.month)+'/'+str(date.year), coef=cpr, intercept=ipr)
         
-    elif metode == "Hubber Regressor" and prediksi == "Harga Dollar":
-        from .model.modelDolarHubber import df, dfe, hub_predict, hub_pred_future
+    elif metode == "Ridge Regression" and prediksi == "Harga Dollar":
+        from .model.modelDolarRidge import df, dfe, rid_predict, rid_pred_future
         fig = Figure(figsize=(10, 8))
         ax = fig.subplots()
         ax.scatter(df.date, df['price'], color='green')
-        ax.plot(df.date, hub_predict)
-        ax.plot(dfe.prediksi, hub_pred_future)
+        ax.plot(df.date, rid_predict)
+        ax.plot(dfe.prediksi, rid_pred_future)
         ax.tick_params(labelrotation=30)
         ax.set_ylabel("Dalam Rupiah")
         ax.set_xlabel("Tanggal (jangka 14 hari)")
-        ax.legend(['Garis Huber Regressor'])
-        ax.set_title("Grafik Huber Regressor Prediksi Harga Dolar")
+        ax.legend(['Garis Ridge Regression'])
+        ax.set_title("Grafik Ridge Regression Prediksi Harga Dolar")
 
         buf = BytesIO()
         fig.savefig(buf, format="png")
 
         data = base64.b64encode(buf.getbuffer()).decode("ascii")
-        return render_template('model.html', data=data, nama=nama, prediksi=prediksi, metode=metode, hasil='Rp. {}'.format(round(float(dh),2)), date=str(date.strftime("%A"))+', '+str(date.day)+'/'+str(date.month)+'/'+str(date.year), coef=cdh, intercept=idh)
-    """
+        return render_template('model.html', data=data, prediksi=prediksi, metode=metode, hasil='Rp. {}'.format(float(dr)), date=str(date.strftime("%A"))+', '+str(date.day)+'/'+str(date.month)+'/'+str(date.year), coef=cdr, intercept=idr)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
