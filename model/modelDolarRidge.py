f2bed076041f7202cda10e6329c416f44893fc30
regiapriandi012 <regiapriandi012@gmail.com>
2021-06-16 15:37:51 +0700

    merubah metode hubber ke ridge

--- model/modelDolarRidge.py
+++ model/modelDolarRidge.py
@@ -1,4 +1,4 @@
-from sklearn.linear_model import HuberRegressor
+from sklearn.linear_model import Ridge
 import numpy as np
 from ..server import data_dolar as data
 import datetime
@@ -12,15 +12,15 @@ price = data["price"]
 df = pd.DataFrame({'date': date, 'price': price})
 df.date  = pd.to_datetime(df.date)
 
-hub = HuberRegressor()
-hub.fit(df.date.values.reshape(-1, 1), df['price'].values.reshape(-1, 1))
+rid = Ridge()
+rid.fit(df.date.values.reshape(-1, 1), df['price'].values.reshape(-1, 1))
 
 date_predict = np.array([str(str(besok.year)+'-0'+str(besok.month)+'-'+str(besok.day))])
 dfe = pd.DataFrame({'prediksi': date_predict})
 dfe.prediksi = pd.to_datetime(dfe.prediksi)
 
-coef = hub.coef_
-intercept = hub.intercept_
+coef = rid.coef_
+intercept = rid.intercept_
 
-hub_predict = hub.predict(df.date.values.astype(float).reshape(-1, 1))
-hub_pred_future = hub.predict(dfe.prediksi.values.astype(float).reshape(-1, 1))
+rid_predict = rid.predict(df.date.values.astype(float).reshape(-1, 1))
+rid_pred_future = rid.predict(dfe.prediksi.values.astype(float).reshape(-1, 1))
