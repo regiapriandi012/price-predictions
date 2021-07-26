e7579ec3962389feb717840ef807ef567c5db10b
regiapriandi012 <regiapriandi012@gmail.com>
2021-07-26 19:43:21 +0700

    add console.sql

---
+++ console.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE tubes_final;
+
+CREATE TABLE emas(
+    date varchar,
+    price int
+);
+
+CREATE TABLE perak(
+    date varchar,
+    price int
+);
+
+CREATE TABLE dolar(
+    date varchar,
+    price int
+);
